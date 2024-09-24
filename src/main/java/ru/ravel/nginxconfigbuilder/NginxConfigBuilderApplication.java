716c18fe80e6e2ae2a9e35afab4c0f87931530e0
petr <petyalom@yandex.ru>
2024-09-24 22:57:55 +0300

    first commit

---
+++ src/main/java/ru/ravel/nginxconfigbuilder/NginxConfigBuilderApplication.java
@@ -0,0 +1,13 @@
+package ru.ravel.nginxconfigbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxConfigBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxConfigBuilderApplication.class, args);
+	}
+
+}
